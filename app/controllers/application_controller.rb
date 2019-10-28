2ab2385c20c4181a322c5a72ef4ae61bf82e8184
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-10-28 16:26:38 -0400

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < Sinatra::Base
 
   configure do
   	set :views, "app/views"
-  	set :public_dir, "public"
+    set :public_dir, "public"
+    set :dog, "app/views/dog"
   end
 
   get "/" do
   	erb :index
+  end
+
+  get "/dogs" do
+    erb :dog
   end
 end
